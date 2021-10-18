93724b7bcd5ece7da2fee3f546eb4a6383c2f2b4
Tylor Wurz <4ciiDicBa55@gmail.com>
2021-10-18 08:42:53 -0500

    getting setup

---
+++ src/test/java/tech/tylor/MapsApp/MapsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.tylor.MapsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
